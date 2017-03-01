b8d1b49135377c60dfec7e1bdbbc55a62d68b134
Yoann Pageaud <yoann.pageaud@gmail.com>
2017-03-01 02:35:18 +0100

    New SQL File and DB Diagram.

--- M2BI_projet_DB_SQL_code.sql
+++ M2BI_projet_DB_SQL_code.sql
@@ -2,11 +2,14 @@
 
 create database M2BI_Projet_PPII;
 
+
 #Display Databases	
 show databases; 
 
+
 #Choose the database M2BI_Projet_PPII
 use M2BI_Projet_PPII;
+
 
 #Tables creation
 
@@ -21,25 +24,23 @@ create table PDB(
 	header VARCHAR(255) not null
 );
 
-create table methodes_analyse(
-	nom_Analyse VARCHAR(7) not null PRIMARY KEY,
-	id_PDB char(4) not null,
-	FOREIGN KEY (id_PDB) REFERENCES PDB(id_PDB) 
-);
-
 create table methodes_resolution(
 	methode_Resolution BIT not null PRIMARY KEY,
 	id_PDB char(4) not null,
 	FOREIGN KEY (id_PDB) REFERENCES PDB(id_PDB)
 );
 
-create table structures_secondaires(
-	id_structure_second INT not null AUTO_INCREMENT PRIMARY KEY,
+create table struct_sec(
+	id_struct_sec INT not null AUTO_INCREMENT PRIMARY KEY,
 	structure_Predite TEXT not null,
 	id_PDB char(4) not null,
-	nom_Analyse VARCHAR(7) not null,
-	FOREIGN KEY (id_PDB) REFERENCES PDB(id_PDB),
-	FOREIGN KEY (nom_Analyse) REFERENCES methodes_analyse(nom_Analyse)
+	FOREIGN KEY (id_PDB) REFERENCES PDB(id_PDB)
+);
+
+create table methodes_analyse(
+	nom_Analyse VARCHAR(7) not null PRIMARY KEY,
+	id_struct_sec INT not null,
+	FOREIGN KEY (id_struct_sec) REFERENCES struct_sec(id_struct_sec) 
 );
 
 
@@ -51,7 +52,10 @@ show tables;
 show columns from PDB;
 show columns from methodes_analyse;
 show columns from methodes_resolution;
-show columns from structures_secondaires;
+show columns from struct_sec;
+
+
+#Delete database: drop database database_name;
 
 
 -- Insertion de Données dans les tables
