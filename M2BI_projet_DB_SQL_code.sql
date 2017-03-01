cafab612c692facd75b812666546892124c9b005
Yoann Pageaud <yoann.pageaud@gmail.com>
2017-03-01 23:47:04 +0100

    last version diagram + sql file.

--- M2BI_projet_DB_SQL_code.sql
+++ M2BI_projet_DB_SQL_code.sql
@@ -4,7 +4,7 @@ create database M2BI_Projet_PPII;
 
 
 #Display Databases	
-show databases; 
+show databases;
 
 
 #Choose the database M2BI_Projet_PPII
@@ -13,34 +13,35 @@ use M2BI_Projet_PPII;
 
 #Tables creation
 
+create table methodes_res(
+	meth_Res BIT not null PRIMARY KEY
+);
+
+create table methodes_analyse(
+	nom_Analyse VARCHAR(7) not null PRIMARY KEY
+);
+
 create table PDB(
 	id_PDB char(4) not null PRIMARY KEY,
 	nom_Proteine VARCHAR(255) not null,
+	chaine VARCHAR(10) not null,
 	sequence_Proteine TEXT not null,
 	taille_Proteine FLOAT not null,
 	pourcentage_PPII FLOAT not null,
 	nombre_PPII INT not null, 
 	resolution_PDB FLOAT not null,
-	header VARCHAR(255) not null
-);
-
-create table methodes_resolution(
-	methode_Resolution BIT not null PRIMARY KEY,
-	id_PDB char(4) not null,
-	FOREIGN KEY (id_PDB) REFERENCES PDB(id_PDB)
+	header VARCHAR(255) not null,
+	meth_Res BIT not null,
+	FOREIGN KEY (meth_Res) REFERENCES methodes_res(meth_Res)
 );
 
 create table struct_sec(
 	id_struct_sec INT not null AUTO_INCREMENT PRIMARY KEY,
 	structure_Predite TEXT not null,
 	id_PDB char(4) not null,
-	FOREIGN KEY (id_PDB) REFERENCES PDB(id_PDB)
-);
-
-create table methodes_analyse(
-	nom_Analyse VARCHAR(7) not null PRIMARY KEY,
-	id_struct_sec INT not null,
-	FOREIGN KEY (id_struct_sec) REFERENCES struct_sec(id_struct_sec) 
+	nom_Analyse VARCHAR(7) not null,
+	FOREIGN KEY (id_PDB) REFERENCES PDB(id_PDB),
+	FOREIGN KEY (nom_Analyse) REFERENCES methodes_analyse(nom_Analyse) 
 );
 
 
@@ -51,7 +52,7 @@ show tables;
 #Show attributes of tables:
 show columns from PDB;
 show columns from methodes_analyse;
-show columns from methodes_resolution;
+show columns from methodes_res;
 show columns from struct_sec;
 
 
