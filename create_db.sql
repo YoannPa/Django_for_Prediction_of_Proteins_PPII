fa826fe4dbf45aed4691e162197168ce170b25c3
Yoann Pageaud <yoann.pageaud@gmail.com>
2017-03-22 13:09:31 +0100

    New SQl file.

---
+++ create_db.sql
@@ -0,0 +1,33 @@
+
+create table methodes_res(
+	meth_Res VARCHAR(10) not null PRIMARY KEY
+);
+
+create table methodes_analyse(
+	nom_Analyse VARCHAR(7) not null PRIMARY KEY
+);
+
+create table PDB(
+	id_PDB CHAR(4) not null PRIMARY KEY,
+	nom_Proteine VARCHAR(255) not null,
+	chaine VARCHAR(10) not null,
+	sequence_Proteine TEXT not null,
+	taille_Proteine INT not null,
+	resolution_PDB FLOAT not null,
+	header VARCHAR(255) not null,
+	meth_Res VARCHAR(10) not null,
+	FOREIGN KEY (meth_Res) REFERENCES methodes_res(meth_Res)
+);
+
+create table struct_sec(
+	id_struct_sec INT not null AUTO_INCREMENT PRIMARY KEY,
+	structure_Predite TEXT not null,
+	nombre_PPII INT not null,
+	pourcentage_PPII FLOAT not null, 
+	angle_phi TEXT not null,
+	angle_psi TEXT not null,
+	id_PDB CHAR(4) not null,
+	nom_Analyse VARCHAR(7) not null,
+	FOREIGN KEY (id_PDB) REFERENCES PDB(id_PDB),
+	FOREIGN KEY (nom_Analyse) REFERENCES methodes_analyse(nom_Analyse) 
+);
