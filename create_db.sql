7387a4a65a187b77ed6ca160c3d4222ad5071c8f
LetourneurQuentin <quentin.ltn92@gmail.com>
2017-04-03 00:35:58 +0200

    update of create_db and setting.py

--- create_db.sql
+++ create_db.sql
@@ -1,37 +1,37 @@
-
 -- Database M2BI_Projet_PPII Creation
 
-create table methodes_res(
-	meth_Res VARCHAR(5) not null PRIMARY KEY
+CREATE TABLE methodes_res(
+	meth_Res VARCHAR(5) NOT NULL PRIMARY KEY
 );
 
-create table methodes_analyse(
-	nom_Analyse VARCHAR(7) not null PRIMARY KEY
+CREATE TABLE methodes_analyse(
+	nom_Analyse VARCHAR(7) NOT NULL PRIMARY KEY
 );
 
-create table PDB(
-	id_PDB CHAR(4) not null PRIMARY KEY,
-	chaine VARCHAR(10) not null,
+CREATE TABLE PDB(
+	id_PDB_chain CHAR(5) NOT NULL PRIMARY KEY,
+	id_PDB CHAR (4) NOT NULL,
+	chaine VARCHAR(10) NOT NULL,
 	header VARCHAR(255) NOT NULL,
-	sequence_Proteine TEXT not null,
+	sequence_Proteine TEXT NOT NULL,
 	start_seq INT NOT NULL,
-	taille_Proteine INT not null,
-	resolution_PDB FLOAT not null,
-	meth_Res VARCHAR(10) not null,
+	taille_Proteine INT NOT NULL,
+	resolution_PDB FLOAT NOT NULL,
+	meth_Res VARCHAR(10) NOT NULL,
 	FOREIGN KEY (meth_Res) REFERENCES methodes_res(meth_Res) ON DELETE CASCADE
 );
 
-create table struct_sec(
-	id_struct_sec INT not null AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE struct_sec(
+	id_struct_sec INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	start_pred INT NOT NULL,
-	structure_Predite TEXT not null,
-	nombre_PPII INT not null,
-	pourcentage_PPII FLOAT not null, 
-	angle_phi TEXT not null,
-	angle_psi TEXT not null,
-	id_PDB CHAR(4) not null,
-	nom_Analyse VARCHAR(7) not null,
-	FOREIGN KEY (id_PDB) REFERENCES PDB(id_PDB) ON DELETE CASCADE,
+	structure_Predite TEXT NOT NULL,
+	nombre_PPII INT NOT NULL,
+	pourcentage_PPII FLOAT NOT NULL, 
+	angle_phi TEXT NOT NULL,
+	angle_psi TEXT NOT NULL,
+	id_PDB_chain CHAR(5) NOT NULL,
+	nom_Analyse VARCHAR(7) NOT NULL,
+	FOREIGN KEY (id_PDB_chain) REFERENCES PDB(id_PDB_chain) ON DELETE CASCADE,
 	FOREIGN KEY (nom_Analyse) REFERENCES methodes_analyse(nom_Analyse) ON DELETE CASCADE 
 );
 
